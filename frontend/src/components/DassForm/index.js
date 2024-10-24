9ac09b7e8364ba2a8af2f033236e090266b892ec
harshAswani1109 <ha1109@srmist.edu.in>
2024-10-25 00:44:06 +0530

    project ka may be final commit

--- frontend/src/components/DassForm/index.js
+++ frontend/src/components/DassForm/index.js
@@ -10,7 +10,7 @@ import {
 
 export default function DassForm() {
   const [vitalsData, setVitalsData] = useState(null);
-  const [responses, setResponses] = useState(Array(3).fill(""));
+  const [responses, setResponses] = useState(Array(42).fill(""));
   const [isLoading, setIsLoading] = useState(false); // New state for loading
   const router = useRouter();
 
